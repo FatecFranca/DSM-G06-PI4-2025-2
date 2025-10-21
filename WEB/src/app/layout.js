6e2102d94d3a46dbff4ac65b38520b2ebacde1b6
João Vitor Nicolau <tinc3lle@gmail.com>
2025-10-21 14:56:11 -0300

    Include | Creating folder in the root of the repository for logo.

--- WEB/src/app/layout.js
+++ WEB/src/app/layout.js
@@ -1,28 +1,19 @@
-// import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "./hooks/useAuth";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata = {
   title: "Smart Backpack",
   description: "Smart BackPack",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
+
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
